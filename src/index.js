c5f9c1ebfef621bd947ebde6c8cfbe77499a9798
luck1306 <81683553+luck1306@users.noreply.github.com>
2022-01-01 22:48:22 +0900

    어딘가 이상한 업데이트

--- src/index.js
+++ src/index.js
@@ -7,18 +7,13 @@ function randomFruit(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let fruitList = ['commonfig','strawberry' ,'tomato','pear','blueberry', 'apple', 'banana', 'melon'];
-let random_left;
-let random_right;
-let left_fruit;
-let right_fruit;
+let fruitList = ['commonfig','strawberry' ,'tomato','pear','blueberry', 'apple', 'banana', 'melon'], semifinalList = [], finalList = [];
+let random_left, random_right, left_fruit, right_fruit, count = 0, semicount = 0;
 
 function random_number_friut() {
-    do {
         random_left = randomFruit(0,fruitList.length-1);
         left_fruit = fruitList[random_left];
     
-        random_right;
         do{ random_right = randomFruit(0,fruitList.length-1);   }while (random_left === random_right);
         right_fruit = fruitList[random_right];
         
@@ -26,16 +21,71 @@ function random_number_friut() {
         leftImage.alt = `${left_fruit}`;
         rightImage.src = `./fruitPicture/${right_fruit}.jpg`;
         rightImage.alt = `${right_fruit}`;
-        } while((left_fruit === undefined)&&(right_fruit === undefined));
 }
+
+function random_number_semi() {
+    random_left = randomFruit(0,semifinalList.length-1);
+    left_fruit = semifinalList[random_left];
+
+    do{ random_right = randomFruit(0,semifinalList.length-1);   }while (random_left === random_right);
+    right_fruit = semifinalList[random_right];
+    
+    leftImage.src = `./fruitPicture/${left_fruit}.jpg`;
+    leftImage.alt = `${left_fruit}`;
+    rightImage.src = `./fruitPicture/${right_fruit}.jpg`;
+    rightImage.alt = `${right_fruit}`;
+}
+
+function semifinal() {
+    p.innerHTML = '준결승';
+    console.log("semifinal");
+    random_number_semi();
+}
+
+function final() {
+    p.innerHTML = '결승';
+    console.log("final");
+}
+
 random_number_friut();  
+
 leftImage.addEventListener('click', () => {
-    console.log(left_fruit);
-    right_fruit = undefined;
-    random_number_friut(); 
+    if (count >= 4){
+        if (semicount >= 2){
+            final();
+        }
+        else{
+            semifinal();
+            finalList[semicount++] = left_fruit;
+            semifinalList.splice(random_left, 1);
+            semifinalList.splice(random_right, 1);
+        }
+    }
+    else {
+        semifinalList[count++] = left_fruit;
+        fruitList.splice(random_left,1);
+        fruitList.splice(random_right,1);
+        console.log('next : ',semifinalList);
+        random_number_friut();
+    }
 });
 rightImage.addEventListener('click', () => {
-    console.log(right_fruit);
-    left_fruit = undefined;
-    random_number_friut();
+    if (count >= 4){
+        if (semicount >= 2){
+            final();
+        }
+        else {
+            semifinal();
+            finalList[semicount++] = right_fruit;
+            semifinalList.splice(random_left, 1);
+            semifinalList.splice(random_right, 1);
+        }
+    }
+    else {
+        semifinalList[count++] = right_fruit;
+        fruitList.splice(random_left,1);
+        fruitList.splice(random_right,1);
+        console.log('next : ',semifinalList);
+        random_number_friut();
+    }
 });
